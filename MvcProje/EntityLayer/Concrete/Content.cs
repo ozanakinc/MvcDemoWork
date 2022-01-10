a873ac546a8766e013aa5399d695843bc88285fc
ozanakinc <53042327+ozanakinc@users.noreply.github.com>
2022-01-10 16:17:11 +0300

    MVC basic sample

---
+++ MvcProje/EntityLayer/Concrete/Content.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+
+        [StringLength(100)]
+        public string ContentValue { get; set; }
+
+        public DateTime ContentTime { get; set; }
+
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int? WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+    }
+}
